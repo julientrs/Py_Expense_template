d624b11beec5c8ff92049e2def6f009404b5a650
julien travers <julien.travers@epita.fr>
2021-11-03 20:15:20 +0100

    2 firsts points

--- expense.py
+++ expense.py
@@ -1,4 +1,7 @@
 from PyInquirer import prompt
+from csv import writer
+
+file_name = "expense_report.csv"
 
 expense_questions = [
     {
@@ -16,15 +19,27 @@ expense_questions = [
         "name":"spender",
         "message":"New Expense - Spender: ",
     },
-
 ]
 
-
+def write_in_csv(infos):
+    line = []
+    # Open file in append mode
+    with open(file_name, 'a+', newline='') as write_obj:
+        # Create a writer object from csv module
+        csv_writer = writer(write_obj)
+        # Iterate over infos
+        for (key, value) in infos.items():
+            line.append(value)
+        csv_writer.writerow(line)
+    return True
 
 def new_expense(*args):
     infos = prompt(expense_questions)
-    # Writing the informations on external file might be a good idea ¯\_(ツ)_/¯
-    print("Expense Added !")
-    return True
+    insertion = write_in_csv(infos)
+    if (insertion == True):
+        print("Expense Added !")
+        return True
+    print("Insertion failed !")
+    return False
 
 
--- user.py
+++ user.py
@@ -1,8 +1,39 @@
 from PyInquirer import prompt
-user_questions = [
+from csv import writer, reader
 
+file_name = "users.csv"
+
+user_questions = [
+    {
+        "type":"input",
+        "name":"fullname",
+        "message":"New User - Full Name: ",
+    },
 ]
 
+def write_in_csv(infos):
+    line = []
+    existingLines = []
+    # Read csv to check user already register
+    with open(file_name,'r') as file_check:
+        csv_reader = reader(file_check)
+        for row in csv_reader:
+            existingLines.append(row[0])
+    # Append in csv user if not exists
+    with open(file_name, 'a+', newline='') as write_obj:
+        csv_writer = writer(write_obj)
+        for (key, value) in infos.items():
+            if (value in existingLines):
+                return False
+            line.append(value)
+        csv_writer.writerow(line)
+    return True
+
 def add_user():
-    # This function should create a new user, asking for its name
-    return
+    infos = prompt(user_questions)
+    insertion = write_in_csv(infos)
+    if (insertion == True):
+        print("Expense Added !")
+        return True
+    print("Insertion failed, user already exists !")
+    return False
